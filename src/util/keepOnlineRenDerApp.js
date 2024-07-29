7c715034f6ccc2d48dad6b4160e66e73616ac646
venuscupid <venuscupid9@gmail.com>
2024-07-29 08:57:50 +0700

    change Url keep online

--- src/util/keepOnlineRenDerApp.js
+++ src/util/keepOnlineRenDerApp.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 async function keepOnlineRenDerApp() {
     // Hàm gọi API
         async function fetchDataFromAPI() {
-            const apiUrl = 'https://qanasneaker.onrender.com';
+            const apiUrl = 'https://qana-sneaker.onrender.com';
 
             try {
             const response = await axios.get(apiUrl);
